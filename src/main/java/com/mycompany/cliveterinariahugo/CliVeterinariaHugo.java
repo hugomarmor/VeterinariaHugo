e03fae6e9bc115175d7f4b52691d0822afbfb971
hugomarmor <hugo.marmor@educa.jcyl.es>
2024-01-15 18:50:49 +0100

    Primer commit clinica(arreglar commit)

---
+++ src/main/java/com/mycompany/cliveterinariahugo/CliVeterinariaHugo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cliveterinariahugo;
+
+/**
+ *
+ * @author Equipo
+ */
+public class CliVeterinariaHugo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
